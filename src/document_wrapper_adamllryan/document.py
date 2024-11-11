63f010411b3024d4450fa31c36b736ff38a8b0e2
Adam <adamllryan@gmail.com>
2024-11-10 23:45:28 -0500

    update doc

--- src/document_wrapper_adamllryan/document.py
+++ src/document_wrapper_adamllryan/document.py
@@ -1,14 +1,26 @@
+from typing import Tuple
+
 class Segment:
+    transcript: str
     text: str
+    formatted_text: str
+    speaker: str
     start: float
     end: float
-    frames: list[float]
-    waveform: list[float]
+    timestamp: Tuple[float, float]
+    frames: list[float] # Change
+    waveform: list[float] # Change
 
-    def __init__(self, text: str, start: float, end: float):
-        self.text = text
-        self.start = start
-        self.end = end
+    def __init__(self, args):
+        self.text = args.get("text")
+        self.transcript = self.text
+        self.formatted_text = args.get("formatted_text")
+        self.speaker = args.get("speaker")
+        self.start = args.get("start")
+        self.end = args.get("end")
+        self.timestamp = args.get("timestamp")
+        self.frames = args.get("frames")
+        self.waveform = args.get("waveform")
 
     def __str__(self):
         return self.text
@@ -20,28 +32,33 @@ class Segment:
         return self.start <= ts <= self.end
 
 
+
+
 class Sentence:
-    text: list[Segment]
-    score: float
+    transcript: list[Segment]
+    text: str
     start: float
     end: float
-    embedding: list[float]
+    embeddings: dict[str, list[float]]
+    text_score: float
+    keyframe_score: int
+    aggregated_score: float
 
-    def __init__(self, text: list[Segment] | dict, score: float = None):
-        if isinstance(text, dict):
-            self.text = [Segment(t, start, end) for (start, end), t in text.items()]
+    def __init__(self, args):
+        sentence = args.get("sentence")
+        if sentence:
+            self.transcript = [Segment(s) for s in sentence]
         else:
-            self.text = [Segment(t["text"], t["start"], t["end"]) for t in text]
-        self.score = score
-        self.start = self.text[0].start
-        self.end = self.text[-1].end
-        self.embedding = None
-
-    def assign_embedding(self, embedding: list[float]):
-        self.embedding = embedding
+            return
+        self.text = " ".join([seg.text for seg in self.transcript])
+        self.start = args.get("start")
+        self.end = args.get("end")
+        self.embeddings = args.get("embeddings")
+        self.text_score = args.get("text_score")
+        self.keyframe_score = args.get("keyframe_score")
 
     def __str__(self):
-        return " ".join([seg.text for seg in self.text])
+        return self.text
 
     def __repr__(self):
         return self.__str__()
@@ -50,20 +67,20 @@ class Sentence:
         return self.start <= ts <= self.end
 
     def find(self, ts: float):
-        for seg in self.text:
+        for seg in self.transcript:
             if seg.contains(ts):
                 return seg
         return None
+
+    def get_plain_text(self):
+        return " ".join([seg.text for seg in self.transcript])
 
 
 class Document:
     sentences: list[Sentence]
 
-    def __init__(self, sentences: list[Sentence] | dict):
-        if isinstance(sentences, dict):
-            self.sentences = [Sentence(s) for s in sentences.values()]
-        else:
-            self.sentences = [Sentence(s) for s in sentences]
+    def __init__(self, sentences: list[dict]):
+        self.sentences = [Sentence(s) for s in sentences]
 
     def __str__(self):
         return "\n".join([f"({s.start}:{s.end}) - " + str(s) for s in self.sentences])
@@ -86,3 +103,42 @@ class Document:
             if seg:
                 return seg
         return None
+
+    def assign_embeddings(self, embeddings: list):
+        assert len(embeddings) == len(self.sentences)
+
+        for i, emb in enumerate(embeddings):
+            self.sentences[i].embeddings = emb
+
+    def assign_scores(self, text_scores: list, keyframe_scores: list):
+        assert len(text_scores) == len(self.sentences)
+        assert len(keyframe_scores) == len(self.sentences)
+
+        for i, s in enumerate(self.sentences):
+            s.text_score = text_scores[i]
+            s.keyframe_score = keyframe_scores[i]
+
+        # compute the normalized scores
+
+        alpha = 0.5
+
+        text_scores = [s.text_score for s in self.sentences]
+
+        min_text_score = min(text_scores)
+        max_text_score = max(text_scores)
+
+        keyframe_scores = [s.keyframe_score for s in self.sentences]
+
+        min_keyframe_score = min(keyframe_scores)
+        max_keyframe_score = max(keyframe_scores)
+
+        for s in self.sentences:
+            s.aggregated_score = alpha * (s.text_score - min_text_score) / (max_text_score - min_text_score) + \
+                                 (1 - alpha) * (s.keyframe_score - min_keyframe_score) / (max_keyframe_score - min_keyframe_score)
+
+
+
+
+    
+
+
