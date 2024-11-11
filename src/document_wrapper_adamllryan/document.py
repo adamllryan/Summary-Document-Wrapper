14461cd8f6372f2fbd37515681321de140d39b48
Adam <adamllryan@gmail.com>
2024-11-10 23:55:07 -0500

    add attempt at auto raw to sentences utility

--- src/document_wrapper_adamllryan/document.py
+++ src/document_wrapper_adamllryan/document.py
@@ -11,7 +11,7 @@ class Segment:
     frames: list[float] # Change
     waveform: list[float] # Change
 
-    def __init__(self, args):
+    def __init__(self, args) -> None:
         self.text = args.get("text")
         self.transcript = self.text
         self.formatted_text = args.get("formatted_text")
@@ -22,13 +22,13 @@ class Segment:
         self.frames = args.get("frames")
         self.waveform = args.get("waveform")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.text
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
-    def contains(self, ts: float):
+    def contains(self, ts: float) -> bool:
         return self.start <= ts <= self.end
 
 
@@ -44,7 +44,7 @@ class Sentence:
     keyframe_score: int
     aggregated_score: float
 
-    def __init__(self, args):
+    def __init__(self, args) -> None:
         sentence = args.get("sentence")
         if sentence:
             self.transcript = [Segment(s) for s in sentence]
@@ -57,60 +57,60 @@ class Sentence:
         self.text_score = args.get("text_score")
         self.keyframe_score = args.get("keyframe_score")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.text
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
-    def contains(self, ts: float):
+    def contains(self, ts: float) -> bool:
         return self.start <= ts <= self.end
 
-    def find(self, ts: float):
+    def find(self, ts: float) -> Segment | None:
         for seg in self.transcript:
             if seg.contains(ts):
                 return seg
         return None
 
-    def get_plain_text(self):
+    def get_plain_text(self) -> str:
         return " ".join([seg.text for seg in self.transcript])
 
 
 class Document:
     sentences: list[Sentence]
 
-    def __init__(self, sentences: list[dict]):
+    def __init__(self, sentences: list[dict]) -> None:
         self.sentences = [Sentence(s) for s in sentences]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "\n".join([f"({s.start}:{s.end}) - " + str(s) for s in self.sentences])
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
-    def get_plain_text(self):
+    def get_plain_text(self) -> str:
         return "\n".join([s.get_plain_text() for s in self.sentences])
 
-    def find_sentence(self, ts: float):
+    def find_sentence(self, ts: float) -> Sentence | None:
         for s in self.sentences:
             if s.contains(ts):
                 return s
         return None
 
-    def find_segment(self, ts: float):
+    def find_segment(self, ts: float) -> Segment | None:
         for s in self.sentences:
             seg = s.find(ts)
             if seg:
                 return seg
         return None
 
-    def assign_embeddings(self, embeddings: list):
+    def assign_embeddings(self, embeddings: list) -> None:
         assert len(embeddings) == len(self.sentences)
 
         for i, emb in enumerate(embeddings):
             self.sentences[i].embeddings = emb
 
-    def assign_scores(self, text_scores: list, keyframe_scores: list):
+    def assign_scores(self, text_scores: list, keyframe_scores: list) -> None:
         assert len(text_scores) == len(self.sentences)
         assert len(keyframe_scores) == len(self.sentences)
 
@@ -136,9 +136,22 @@ class Document:
             s.aggregated_score = alpha * (s.text_score - min_text_score) / (max_text_score - min_text_score) + \
                                  (1 - alpha) * (s.keyframe_score - min_keyframe_score) / (max_keyframe_score - min_keyframe_score)
 
-
-
-
-    
-
-
+    @staticmethod
+    def raw_to_sentences(raw: list[dict]) -> list[Sentence]:
+        # from raw transcript, create sentences by checking for periods as the last character
+        # assumes that each dict is {text: str, 'timestamp': (float, float), 'speaker': str, 'start': float, 'end': float, 'formatted_text': str}
+        
+        sentences = []
+        sentence = []
+        for r in raw:
+            sentence.append(r)
+            if r['text'][-1] == '.':
+                sentences.append(Sentence({'sentence': sentence, 
+                                           'start': sentence[0]['start'],
+                                             'end': sentence[-1]['end']}))
+                sentence = []
+        if sentence:
+            sentences.append(Sentence({'sentence': sentence, 
+                                       'start': sentence[0]['start'],
+                                         'end': sentence[-1]['end']}))
+        return sentences
