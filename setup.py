847918df1e8dc7fa3723f8a75e877bf66ee03bdc
Adam <adamllryan@gmail.com>
2025-01-31 22:56:15 -0500

    readd?

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="document_wrapper_adamllryan",
+    version="0.1",
+    packages=find_packages(where="document_wrapper_adamllryan"),
+    install_requires=[
+        "yt-dlp",
+        'pyannote.audio',
+        'ujson',
+        'sentence_transformers',
+        'moviepy',
+        'spacy',
+        'torch',
+    ]
+)
