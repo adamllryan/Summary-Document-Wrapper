6f45622b21c2b3699de02f2a1ce2a1660cf14c87
Adam <adamllryan@gmail.com>
2025-01-31 22:29:02 -0500

    explicit mention of package

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="document_wrapper_adamllryan",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages([
+        "document_wrapper_adamllryan",
+    ]),
     install_requires=[
         "yt-dlp",
         "rouge"
