1cf69524dbfcaf1b7994a5e4f4cf353fd965084d
Adam <adamllryan@gmail.com>
2025-02-15 21:22:45 -0500

    Update summarizer.py

--- src/document_wrapper_adamllryan/analysis/summarizer.py
+++ src/document_wrapper_adamllryan/analysis/summarizer.py
@@ -48,33 +48,101 @@ class Summarizer:
         """Summarizes input text by breaking it into chunks that fit within the token limit."""
 
         print("Summarizing")
+        # print(f"Text to summarize: {text}\n----")
 
         """ Break into sentences """
-        chunks = text.split("\n")
-        
-        summary = ""
-        i = 0
 
         chunks = [""]
-        for chunk in text.split("\n"):
-          if len(self.token_counter.tokenize(chunks[-1])) + len(self.token_counter.tokenize(chunk)) < self.config["token_limit"]:
-            chunks[-1] += chunk + "\n"
+
+        for sentence in text.split("\n"):
+          # print(f"Sentence to chunk: {sentence}")
+          tokens = self._count_tokens(sentence)
+          current_chunk_len = self._count_tokens(chunks[-1])
+          # Need to add this because whisper has been making lower quality
+          # content over time, less properly formatted
+          if tokens > self.config["token_limit"]:
+            # print("Subsentence splitting")
+            # If a sentence is larger than token limit we break down further
+            remaining_size = self.config["token_limit"] - current_chunk_len - 1
+            subsentences = self._split_large_sentences(sentence, self.config["token_limit"] - 1, remaining_size)
+            for subsentence in subsentences:
+              sub_length = self._count_tokens(subsentence)
+              current_chunk_len = self._count_tokens(chunks[-1])
+              if sub_length + current_chunk_len < self.config["token_limit"] - 1:
+                chunks[-1]+=subsentence + "\n"
+              else:
+                chunks.append(subsentence + "\n")
+            # print(f"Got subsentences:")
+            # for subsentence in subsentences:
+            #   print(f"{subsentence}")
+            # print(f"Chunks as of subsentence: {chunks}")
+          elif tokens + current_chunk_len < self.config["token_limit"]:
+            # Extend the sentence if we are still under the len of token limit
+            chunks[-1] += sentence + "\n"
+            # print(f"Extending chunk: {chunks[-1]}")
           else:
-            chunks.append(chunk + "\n")
+            # Create a new chunk
+            chunks.append(sentence + "\n")
+            # print(f"Creating new chunk: {chunks[-1]}")
 
+        # print(f"---\nChunks: {chunks}\n---")
 
-        for chunk in chunks:
-            summary += self._generate_summary(chunk)
+        summary = self._generate_summary(chunks)
+        # print(f"Summary: {summary}")
 
 
         return summary
 
-    def _generate_summary(self, text: str) -> str:
+    def _generate_summary(self, text: list[str] | str) -> str:
         """Generates a summary for a given text chunk."""
-        assert len(self.token_counter.tokenize(text)) <= self.config["token_limit"]
-        return self.summarizer([text], max_length=256, min_length=30, do_sample=False)[0]['summary_text']
+        if isinstance(text, str):
+          text = [text]
+        for chunk in text:
+          # print(f"Tokenizing chunk: {chunk}")
+          length = self._count_tokens(chunk)
+          assert length <= self.config["token_limit"], f"Length was {length}"
+        return self.summarizer(text, max_length=self.config["max_len"], min_length=self.config["min_len"], do_sample=False)[0]['summary_text']
 
     def _count_tokens(self, text: str) -> int:
         """Counts the number of tokens in a given text."""
 
         return len(self.token_counter.tokenize(text))
+
+
+    def _split_large_sentences(self, sentence: str, max_tokens: int, remaining_size: int) -> List[str]:
+        """Splits a sentence into smaller chunks that fit within the token limit."""
+        words = [word.strip() for word in sentence.split(" ")]
+        print(f"words: {words}\n---")
+        current_token_count = 0
+        subsentences = [""]
+        # Grab the remaining words that fit in previous chunk
+
+
+
+        for word in words:
+            word_token_len = self._count_tokens(word + " ")
+
+            # If we want to fill remaining size first
+            if len(subsentences) == 1:
+              if current_token_count + word_token_len < remaining_size:
+                subsentences[0] += word + " "
+                current_token_count += word_token_len
+              else:
+                subsentences.append(word + " ")
+                current_token_count = word_token_len
+                # Continue splitting normally
+            else:
+              if current_token_count + word_token_len < max_tokens:
+                subsentences[-1] += word + " "
+                current_token_count += word_token_len
+              else:
+                subsentences.append(word + " ")
+                current_token_count = word_token_len
+        print(f"Subsentences: {subsentences}\n---")
+        return subsentences
+
+
+
+
+
+
